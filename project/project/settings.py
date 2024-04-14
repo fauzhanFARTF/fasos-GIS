142aaa76efbdd8627efbb6b31ac0c9c44221583c
fauzhanFARTF <fauzan1812@gmail.com>
2024-04-14 22:17:06 +0700

    Create Static Root, Static URL, Media Root, dan Media URL

--- project/project/settings.py
+++ project/project/settings.py
@@ -135,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "assets/static"
+MEDIA_URL = 'media/'
+MEDIA_ROOT = "assets/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
