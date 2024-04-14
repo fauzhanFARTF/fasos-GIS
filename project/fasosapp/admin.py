e7945ec4d964131262b31ae33325f7cd9ea9c125
fauzhanFARTF <fauzan1812@gmail.com>
2024-04-15 00:58:00 +0700

    Create Class MedicalFacility in models.py && Create Class LocationAdmin, MedicalFacilityAdmin(LocationAdmin), @admin.register(MedicalFacility) in admin.py

--- project/fasosapp/admin.py
+++ project/fasosapp/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# from django.contrib import admin      # Admin Non Spatial
+from django.contrib.gis  import admin   # Admin Spatial
+from .models import MedicalFacility
 
 # Register your models here.
+class LocationAdmin(admin.OSMGeoAdmin):
+    default_zoom = 10
+    default_lon = 11859737.83338
+    default_lat = -696023.89962
+
+@admin.register(MedicalFacility)
+class MedicalFacilityAdmin(LocationAdmin):
+    list_filter = ['status', 'operator'] 
+    list_display = ['id', 'nama','jenis','tingkatan','status', 'operator']
+
+# admin.site.register(MedicalFacility)
+# class MedicalFacilityAdmin(LocationAdmin):
+#     list_filter = ['status', 'operator']
+#     list_display = ['id', 'nama','jenis','tingkatan','status', 'operator']
--- project/fasosapp/models.py
+++ project/fasosapp/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+# from django.db import models # Model Database NonSpasial
+from django.contrib.gis.db import models # Model Database Spasial
+from django.contrib.auth.models import User
 
 # Create your models here.
+class MedicalFacility(models.Model) :
+    TYPE_CHOICES =[
+        ('Rumah Sakit','Rumah Sakit'),
+        ('Puskesmas','Puskesmas'),
+        ('Klinik', 'Klinik'),
+        ('Apotik', 'Apotik')
+    ]
+    
+    STATUS_CHOICES = [
+        ('Perencanaan/Pengajuan', 'Perencanaan/Pengajuan'),
+        ('Dalam Masa Peninjauan', 'Dalam Masa Peninjauan'),
+        ('Perencanaan Dibatalkan', 'Perencanaan Dibatalkan'),
+        ('Dalam Masa Pembangunan', 'Dalam Masa Pembangunan'),
+        ('Pembangunan Selesai/Belum Beroperasi', 'Pembangunan Selesai/Belum Beroperasi'),
+        ('Pembangunan Selesai/Sudah Beroperasi', 'Pembangunan Selesai/Sudah Beroperasi'),
+        ('Tutup/Sudah Tidak Beroperasi', 'Tutup/Sudah Tidak Beroperasi')
+    ]
+    
+    LEVEL_CHOICES = [
+        ('Fasilitas Kesehatan Tingkat 1', 'Fasilitas Kesehatan Tingkat 1'),
+        ('Fasilitas Kesehatan Tingkat 2', 'Fasilitas Kesehatan Tingkat 2'),
+        ('Fasilitas Kesehatan Tingkat 3', 'Fasilitas Kesehatan Tingkat 3')
+    ]
+    
+    DAYS_CHOICES = [
+        ('Setiap Hari', 'Setiap Hari'),
+        ('Senin - Jumat', 'Senin - Jumat'),
+        ('Sabtu - Minggu', 'Sabtu - Minggu')
+    ]
+    
+    nama = models.CharField(max_length=50)
+    jenis = models.CharField(max_length=30, choices=TYPE_CHOICES, default='Rumah Sakit')
+    tingkatan = models.CharField(max_length=50, choices=LEVEL_CHOICES)
+    alamat = models.TextField(max_length=255)
+    no_telp = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Perencanaan/Pengajuan')
+    # beroperasi = models.BooleanField(default=False)
+    hari_beroperasi = models.CharField(max_length=50, choices=DAYS_CHOICES)
+    jam_beroperasi = models.CharField(max_length=50)
+    location = models.PointField(srid=4326, spatial_index=True)
+    photo = models.ImageField(upload_to='medical_facility')
+    operator = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_field = models.DateTimeField(auto_now=True)
