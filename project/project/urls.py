4bc8108d6fe66e2f0b95abd20db13ac740cdf39c
fauzhanFARTF <fauzan1812@gmail.com>
2024-04-20 02:44:43 +0700

    Create Navbar and create pathname = 'home'

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
