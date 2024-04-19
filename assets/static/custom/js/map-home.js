4a265c306357ab23e76551d38f9cfbb7dcee754e
fauzhanFARTF <fauzan1812@gmail.com>
2024-04-20 04:29:51 +0700

    Create Configuration Leaflet Static, Create folder Custom/js and Custom/css

---
+++ assets/static/custom/js/map-home.js
@@ -0,0 +1,6 @@
+// MAP HOME
+const map = L.map('peta').setView([-6.1950,106.5528], 11);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
