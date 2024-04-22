85e6dd5fe21ff094a8fb61bd95b6b6a9ca4f5ad9
fauzhanFARTF <fauzan1812@gmail.com>
2024-04-22 12:29:24 +0700

    Add Control Zoom Bar in Map

--- assets/static/custom/js/plugins.js
+++ assets/static/custom/js/plugins.js
@@ -5,3 +5,17 @@ let ctlMousePosition = L.control.mousePosition({
 
 // Control Pan
 let ctlPan = L.control.pan({position: "bottomleft"}).addTo(map);
+
+// Implementation: Set native zoomControl option to false!
+//  ZoomBar uses the initial map view as the home position.
+    let zoom_bar = new L.Control.ZoomBar({position: 'topleft'}).addTo(map);
+    L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+        {maxZoom: 11}).addTo(map);
+    
+    function getCenterAndZoom(){
+    //    console.log("Lat: "+map.getCenter().lat);
+    //    console.log("Lon: "+map.getCenter().lng);
+    //    console.log("Zoom: "+map.getZoom());
+        alert("Lat: "+map.getCenter().lat+"\nLon: "+map.getCenter().lng+"\nZoom: "+map.getZoom());
+    }
