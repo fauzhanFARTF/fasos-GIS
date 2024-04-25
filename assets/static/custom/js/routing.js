005c6107709a2363ebd9814f5941e38a904d8abb
fauzhanFARTF <fauzan1812@gmail.com>
2024-04-25 12:20:03 +0700

    Add graphhopper plugins, modified routing, move custom styles. css

--- assets/static/custom/js/routing.js
+++ assets/static/custom/js/routing.js
@@ -1,4 +1,15 @@
-// Routing Machine
+// Routing Basic Example
+    /*
+    L.Routing.control({
+        waypoints: [
+            L.latLng(-6.270112, 106.484313),
+            L.latLng(-6.224910, 106.506233)
+        ],
+        routeWhileDragging: true
+    }).addTo(map);
+    */
+
+// Routing Machine Basic
     const control = L.Routing.control({
         language: 'id',
         formatter:  new L.Routing.Formatter({
@@ -23,17 +34,27 @@
     })
     control.addTo(map);
 
-    /*
-    L.Routing.control({
-        waypoints: [
-            L.latLng(-6.270112, 106.484313),
-            L.latLng(-6.224910, 106.506233)
-        ],
-        routeWhileDragging: true
-    }).addTo(map);
-    */
+// Routing use GraphHopper
+    // var routingControl = L.Routing.control({
+    //     waypoints: [
+    //         L.latLng(-6.222045, 106.560920),
+    //         L.latLng(-6.197103, 106.439644)
+    //     ],
+    //     geocoder: L.Control.Geocoder.nominatim(),
+    //     router: L.Routing.graphHopper('4a404492-6e4e-4237-bb8d-645ee9fafe67'),
+    //     routeWhileDragging: false
+    // }).addTo(map);
+
+    // var router = routingControl.getRouter();
+    // router.on('response',function(e){
+    // console.log('This request consumed ' + e.credits + ' credit(s)');
+    // console.log('You have ' + e.remaining + ' left');
+    // });
+
 
     function keSini(Lat,Lng){
         let latLng = L.latLng(Lat, Lng)
         control.spliceWaypoints(control.getWaypoints().length - 1, 1, latLng)
     }
+
+    
